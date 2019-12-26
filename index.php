d15813a7eca824f7cd637cf2d07d0384d134edd7
Eric Bai <eric@ericbai.com>
2019-12-25 17:35:57 -0800

    updated build script, cleaned up index template, removed page-specific styles

--- index.php
+++ index.php
@@ -1,20 +1,11 @@
 
 <?php get_header(); ?>
 
-<h1>Hello world!</h1>
-
 <?php
 while (have_posts()) {
     the_post();
 ?>
-    <div class="title">
-        <h2>Title</h2>
-        <?php the_title(); ?>
-    </div>
-    <div class="content">
-        <h3>Content</h3>
-        <?php the_content(); ?>
-    </div>
+    <?php the_content(); ?>
 <?php
 }
 ?>
