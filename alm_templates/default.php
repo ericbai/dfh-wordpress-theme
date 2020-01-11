e3e5cbd3d57e9f42e7580ac0d22f4cac66399c9f
Eric Bai <eric@ericbai.com>
2020-01-10 20:06:13 -0500

    moved nested block helper functions to plugin

--- alm_templates/default.php
+++ alm_templates/default.php
@@ -3,6 +3,13 @@
 // see https://connekthq.com/plugins/ajax-load-more/docs/repeater-templates/#theme
 // see https://developer.wordpress.org/themes/references/list-of-template-tags/
 $id = get_the_ID();
+
+// function definitions loaded in DFH plugin
+if (!function_exists('dfh_get_block') || !function_exists('dfh_pluralize')) {
+    echo 'Unable to display item. Please make sure the Docs for Health plugin is installed.';
+    echo '<br />';
+    return;
+}
 ?>
 
 <?php if (get_post_type($id) == DFH_CONTENT_TYPE_RESOURCE && has_block(DFH_REQUIRED_BLOCK_RESOURCE, $id)): ?>
